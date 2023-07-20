510f2b809d74a3615a265fbfe717e0c58f28cc30
Ridwan Hanif Abdurrasyid <ridwan.abdurrasyid@yahoo.com>
2023-07-20 13:58:30 +0700

    Initial Commit

---
+++ DotA 2 Heroes/DotA_2_HeroesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DotA_2_HeroesApp.swift
+//  DotA 2 Heroes
+//
+//  Created by Ridwan Abdurrasyid on 20/07/23.
+//
+
+import SwiftUI
+
+@main
+struct DotA_2_HeroesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
